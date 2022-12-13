99538f1a25518e14e95d7e04a666baa7867458cd
Saumay Rustagi <saumay03pro@gmail.com>
2022-12-13 19:59:15 +0530

    w

--- Elixir/test.exs
+++
@@ -1,3 +0,0 @@
-IO.puts("Hello world from Elixir"<>" 4")
-
-IO.puts(is_integer(4))
---
+++ learning_elixir/test.ex
@@ -0,0 +1,7 @@
+defmodule Test do
+	import IO
+	def test do
+		puts("Hello world from Elixir"<>" 4")
+		puts(is_integer(4))
+	end
+end
