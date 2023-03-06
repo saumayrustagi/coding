d952aaf4165a201c8dcbff5368c087c88c592122
Saumay Rustagi <saumay03pro@gmail.com>
2023-03-06 18:44:16 +0530

    w

--- stford/cs107/short_to_double.cpp
+++ stford/cs107/short_to_double.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-	short s = 45;                         // 00000000 00101101
+	short s = 45;                            // 00000000 00101101
 	// int i = *(int *)&s; // xxxxxxxx xxxxxxxx 00000000 00101101
 	double d = *(double *)&s;
 	cout<<s<<' '<<i<<' '<<endl;
