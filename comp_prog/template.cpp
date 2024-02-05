f08a4268f07e936db238d3394ac5738df1a275b5
Saumay Rustagi <saumay03pro@gmail.com>
2024-02-06 00:30:33 +0530

    w

---
+++ comp_prog/template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = int_fast64_t;
+using i32 = int_fast32_t;
+using i16 = int_fast16_t;
+
+int main(){
+	vector<i32> v(5);
+	for(i32& n: v){
+		cout << n << '\n';
+	}
+	return 0;
+}
